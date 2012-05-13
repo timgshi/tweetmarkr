bdf970478a09ad6359b8a6cb58ef433bfeae1b38
Tim Shi <timgshi@gmail.com>
2012-05-12 21:49:32 -0700

    set up to return single tweet

--- tweetmarkrproj/tweetmarkr/views.py
+++ tweetmarkrproj/tweetmarkr/views.py
@@ -1,7 +1,18 @@
 from django.http import HttpResponse
 import json
+import pymongo
+from pymongo import Connection
+from bson import json_util
 
 def tweet(request):
+	mongoConnection = Connection()
+	db = mongoConnection.tweets
+	collection = db.tweets
+	tweets = []
+	for tweet in collection.find().sort('created_at', 1).limit(1):
+		tweets.append(tweet)
+	
 	returnMap = {}
 	returnMap['Name'] = 'Tim'
-	return HttpResponse(json.dumps(returnMap), mimetype="application/json")
+	returnMap['Friend'] = 'Jason'
+	return HttpResponse(json.dumps(tweets, default=json_util.default), mimetype="application/json")
